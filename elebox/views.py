7032d73e481ce274e723ec3c02d53c08b19cb72f
Valber <13department@gmail.com>
2013-10-11 10:40:29 +0400

    simple search by name for radioelement

--- elebox/views.py
+++ elebox/views.py
@@ -2,6 +2,47 @@
 from elebox.models import Corpus , Placekeep, Radioelem, Device
 from django.template import loader, Context
 from django.http import HttpResponse
+# Воспользуемся следующим примером
+# http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
+import re
+from django.db.models import Q
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    '''
+Splits the query string in invidual keywords, getting rid of
+    unecessary spaces and grouping quoted words together.
+
+Example:
+    
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+    ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    '''
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+def get_query(query_string, search_fields):
+    '''
+    Returns a query, that is a combination of Q objects. That combination
+    aims to search keywords within a model by testing the given search fields.
+    
+    '''
+    query = None # Query to search for every search term        
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+    
 # Стартовая страница базы данных
 def index(request):
     t = loader.get_template("start.html")
@@ -20,6 +61,7 @@ def corpusview(request, aname):
     c = Context({'title_page':'Corpus','models':corpus })
     return HttpResponse(t.render(c))
 
+# Страница с местами хранения
 def placeview(request, aname):
     if aname :
         if aname[-1]=='/':
@@ -32,18 +74,30 @@ def placeview(request, aname):
     c = Context({'title_page':'Place','models':places, 'typedata':'places' })
     return HttpResponse(t.render(c))
 
+# Страница с радиоэлектронными компонентами
 def element(request, aname):
-    if aname :
-        if aname[-1]=='/':
-            aname=aname[:-1]
-        elements= Radioelem.objects.filter(name=aname) 
+    print aname
+    elements = None
+    query_string = ''
+    if (request.GET):
+        if ('q' in request.GET) and request.GET['q'].strip():
+            query_string = request.GET['q']
+            entry_query = get_query(query_string, ['name', 'manufacturer', 'typedev',])
+            elements = Radioelem.objects.filter(entry_query)    
+    elif (request.POST):
+        print request.POST
     else:
-        elements= Radioelem.objects.all() 
+        if aname :
+            if aname[-1]=='/':
+                aname=aname[:-1]
+            elements= Radioelem.objects.filter(name=aname) 
+        else:
+            elements= Radioelem.objects.all() 
     t = loader.get_template("element.html")
     c = Context({'title_page':'Моё барахло','models':elements })
     return HttpResponse(t.render(c))
 
-
+# Страница устройств
 def device(request, aname):
     if aname :
         if aname[-1]=='/':
